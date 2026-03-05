588256e9ea72633deb7de162b3287303e06a0dbf
Angela <anglopoli1@alum.us.es>
2026-03-05 18:46:56 +0100

    Implement room and hall management features with routing and styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,55 +1,16 @@
-import useFetch from "./hooks/useFetch";
-import Calendario from "./components/salas/Calendario";
-import './App.css'
+import { Link } from "react-router-dom";
 
-function App() {
-  const { data, loading, error } = useFetch("http://127.0.0.1:8000/habitaciones/");
-  const { data: salas, loading: loadingSalas, error: errorSalas } = useFetch("http://127.0.0.1:8000/salas/");
-
-  if(loading || loadingSalas)
-    return <p>Cargando ...</p>;
-  if(error)
-    return <p>Error {error}</p>;
-  if(errorSalas)
-    return <p>Error {errorSalas}</p>;
-
+export default function App() {
   return (
-    <>
-      <h1>Hotel Miga</h1>     
+    <div>
+      <h1>Hotel Miga</h1>
 
-      {data?.map((habitacion) => (
-        <article key={habitacion.id}>
-          <h2>Número de habitación: {habitacion.num_habitacion}</h2>
-          <p>Precio por noche: {habitacion.precio}</p>
-          <p>Número de personas: {habitacion.num_personas}</p>
-          <p>{habitacion.descripcion}</p>
-          <h3>Extras</h3>
-          <ul>
-            {habitacion.extras?.map((extra) => (
-              <li key={extra.id}>{extra.nombre}</li>
-            ))}
-          </ul>
-        </article>       
-      ))}
-
-      {salas?.map((sala) => (
-        <article key={sala.id}>
-          <h2>Nombre de la sala: {sala.nombre_sala}</h2>
-          <p>Precio: {sala.precio}</p>
-          <p>Aforo máximo: {sala.personas_max}</p>
-          <p>{sala.descripcion}</p>
-          <h3>Extras</h3>
-          <ul>
-            {sala.extras?.map((extra) => (
-              <li key={extra.id}>{extra.nombre}</li>
-            ))}
-          </ul>
-        </article>       
-      ))}
-      
-      <Calendario sala={salas[0]}/>
-    </>    
-  )
+      <Link to="/habitaciones">
+        <button>Ver habitaciones</button>
+      </Link>
+      <Link to="/salas">
+        <button>Ver salas y habitaciones</button>
+      </Link>
+    </div>
+  );
 }
-
-export default App
