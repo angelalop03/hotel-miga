9c14059d828c65a899925e05e80367a35849cc4a
Míchel Acosta <michelinho80@gmail.com>
2026-03-06 17:37:48 +0000

    Add available room search endpoint and ReservaHabitacion model

--- backend/habitacion/urls.py
+++ backend/habitacion/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import HabitacionViewSet
+from .views import HabitacionViewSet, HabitacionesDisponiblesAPIView
 
 router = DefaultRouter()
 router.register('', HabitacionViewSet, basename='habitacion-viewset')
 
-urlpatterns = router.urls
+urlpatterns = [   
+    path(
+        "disponibles/",
+        HabitacionesDisponiblesAPIView.as_view(),
+        name="habitaciones-disponibles"
+    )
+] + router.urls
--- backend/reservas/serializer.py
+++ backend/reservas/serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import ReservaSala
+from .models import ReservaSala, ReservaHabitacion
 
 # Serializador para crear y eliminar reservas de salas, pero no cambiar su estado
 class ReservaSalaSerializer(serializers.ModelSerializer):
@@ -24,3 +24,27 @@ class ReservaSalaOcupadoSerializer(serializers.ModelSerializer):
     class Meta:
         model = ReservaSala
         fields = ["fecha", "horario"]
+
+# Serializador para crear y eliminar reservas de habitaciones, pero no cambiar su estado
+class ReservaHabitacionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ReservaHabitacion
+        fields = ["id", "id_habitacion", "fecha_entrada", "fecha_salida", "num_personas", "nombre", "email", "telefono", "estado"]
+        read_only_fields = ["estado"]
+    
+    # Añadir el numero de la habitacion al GET
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        rep["num_habitacion"] = instance.id_habitacion.num_habitacion
+        return rep
+
+# Serializador solo para cambiar el estado de una sala
+class ReservaHabitacionEstadoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ReservaHabitacion
+        fields = ["estado"]
+
+class ReservaHabitacionOcupadoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ReservaHabitacion
+        fields = ["fecha_entrada", "fecha_salida"]
